ab21236b9748cf5c7afda9ed43321c48d579dbdf
Victor Arreaga <arreagav17@gmail.com>
2022-06-22 18:24:25 -0500

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('@modules/home/home.module').then(m => m.HomeModule),
+  },
+  {
+    path: 'category',
+    loadChildren: () =>
+      import('@modules/category/category.module').then(m => m.CategoryModule),
+  },
+  {
+    path: 'product',
+    loadChildren: () =>
+      import('@modules/product/product.module').then(m => m.ProductModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
