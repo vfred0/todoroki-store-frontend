ada00ec0008c4e806fa5f5846ad071b1575bb0ee
Victor Arreaga <arreagav17@gmail.com>
2022-06-26 12:49:04 -0500

    Deleted styles

--- src/app/modules/home/page/home-page.component.ts
+++ src/app/modules/home/page/home-page.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import SwiperCore, { Autoplay, Pagination, Navigation, Parallax } from 'swiper';
-
-SwiperCore.use([Parallax, Autoplay, Pagination, Navigation]);
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
   constructor() {}
