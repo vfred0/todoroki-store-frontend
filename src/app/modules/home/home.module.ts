4c1dab9030de38154ad98534ed5be36c022fc8b7
Victor Arreaga <arreagav17@gmail.com>
2022-06-26 12:47:46 -0500

    Added components

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomePageComponent } from './page/home-page/home-page.component';
+import { HomePageComponent } from './page/home-page.component';
+import { SwiperModule } from 'swiper/angular';
+import { SliderComponent } from './components/slider/slider.component';
+import { MostWantedContentComponent } from './components/most-wanted-content/most-wanted-content.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [
+    HomePageComponent,
+    SliderComponent,
+    MostWantedContentComponent,
+  ],
+  imports: [CommonModule, HomeRoutingModule, SwiperModule, SharedModule],
 })
 export class HomeModule {}
