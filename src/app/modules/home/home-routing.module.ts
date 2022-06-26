2e22f0389278709304b5efe4324b884420bda389
Victor Arreaga <arreagav17@gmail.com>
2022-06-26 12:47:14 -0500

    Fix route home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './page/home-page/home-page.component';
+import { HomePageComponent } from './page/home-page.component';
 
 const routes: Routes = [
   {
