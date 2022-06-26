b7061773176b1a6f43978b894026a84dcbb3f08c
Victor Arreaga <arreagav17@gmail.com>
2022-06-26 12:45:12 -0500

    Added export button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, ButtonComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, ButtonComponent],
 })
 export class SharedModule {}
