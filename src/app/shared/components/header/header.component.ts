bcb2a4c44f349d3955ec7ad39389ec4b59b82861
Victor Arreaga <arreagav17@gmail.com>
2022-06-26 17:27:17 -0500

    Added onToggle

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TypeButton } from '@core/models/TypeButton';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  activeModal: string = '';
+  typeButton: TypeButton = TypeButton.Default;
   constructor() {}
+
+  onToggle(event: any): void {
+    event.preventDefault();
+    if (this.activeModal === 'is-active') {
+      this.activeModal = '';
+    } else {
+      this.activeModal = 'is-active';
+    }
+  }
 }
