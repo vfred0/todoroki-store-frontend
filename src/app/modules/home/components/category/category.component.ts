e170571dbcded97cf5006f8ed24f5ccd707a892b
Victor Arreaga <arreagav17@gmail.com>
2022-06-27 08:29:42 -0500

    Added inputs name and quantity

--- src/app/modules/home/components/category/category.component.ts
+++ src/app/modules/home/components/category/category.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
 })
 export class CategoryComponent {
+  @Input() nameCategory: string = 'Camisetas';
+  @Input() quantity: number = 10;
   constructor() {}
 }
