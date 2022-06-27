866957d076a3aeecfcc224e780c10f37cf4bc9c8
Victor Arreaga <arreagav17@gmail.com>
2022-06-27 08:24:37 -0500

    Created card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [HeaderComponent, ButtonComponent],
+  declarations: [HeaderComponent, ButtonComponent, CardComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, ButtonComponent],
 })
