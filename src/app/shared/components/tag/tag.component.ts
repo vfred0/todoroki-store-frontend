bba4faba0e38127b5d9b4427cc3520e3242d251d
Victor Arreaga <arreagav17@gmail.com>
2022-06-27 15:16:06 -0500

    Create component

---
+++ src/app/shared/components/tag/tag.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tag',
+  templateUrl: './tag.component.html'
+})
+export class TagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
