28ad4303a441d3efbf71fdfc28a90257c7e56d85
Victor Arreaga <arreagav17@gmail.com>
2022-06-30 12:01:33 -0500

    Added logic when is selected

--- src/app/shared/components/tag/tag.component.ts
+++ src/app/shared/components/tag/tag.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { Tag, TypeTag } from '@core/utils/Tag';
 
 @Component({
   selector: 'app-tag',
-  templateUrl: './tag.component.html'
+  templateUrl: './tag.component.html',
 })
-export class TagComponent implements OnInit {
+export class TagComponent implements AfterViewInit {
+  @Input() tag: Tag = {
+    typeTag: TypeTag.Likes,
+    description: '150',
+    pathIcon: 'assets/icons/heart.svg',
+    isClickable: true,
+  };
+  @ViewChild('etag') _elementTag: ElementRef = new ElementRef('');
+  _selected: string = '';
 
-  constructor() { }
+  @Output() selected: EventEmitter<boolean> = new EventEmitter();
 
-  ngOnInit(): void {
+  constructor(private renderer2: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    if (this.tag.isClickable) {
+      this.renderer2.listen(
+        this._elementTag.nativeElement,
+        'click',
+        (event: any) => {
+          event.preventDefault();
+          if (this._selected === 'has-selected') {
+            this._selected = '';
+          } else {
+            this._selected = 'has-selected';
+            this.selected.emit(true);
+          }
+        }
+      );
+    }
   }
-
 }
