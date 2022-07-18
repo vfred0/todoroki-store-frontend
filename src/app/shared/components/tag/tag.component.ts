e4f4e907397842530ed4ac6ee9b7c8ca2286646a
Victor Arreaga <arreagav17@gmail.com>
2022-07-18 16:58:06 -0500

    Refactor

--- src/app/shared/components/tag/tag.component.ts
+++ src/app/shared/components/tag/tag.component.ts
@@ -8,39 +8,37 @@ import {
   Renderer2,
   ViewChild,
 } from '@angular/core';
-import { Tag, TypeTag } from '@core/utils/Tag';
+import { Tag } from '@core/utils/Tag';
 
 @Component({
   selector: 'app-tag',
   templateUrl: './tag.component.html',
 })
 export class TagComponent implements AfterViewInit {
-  @Input() tag: Tag = {
-    typeTag: TypeTag.Likes,
-    description: '150',
-    pathIcon: 'assets/icons/heart.svg',
-    isClickable: true,
-  };
-  @ViewChild('etag') _elementTag: ElementRef = new ElementRef('');
-  _selected: string = '';
+  @Output() selectionOfTag: EventEmitter<Tag>;
+  @Input() selectable: boolean;
+  @Input() tag: Tag;
+  @Input() styles: string;
+  @Input() isSelected: boolean;
 
-  @Output() selected: EventEmitter<boolean> = new EventEmitter();
-
-  constructor(private renderer2: Renderer2) {}
+  @ViewChild('elementTag') elementTag: ElementRef;
+  constructor(private renderer2: Renderer2) {
+    this.isSelected = false;
+    this.tag = {} as Tag;
+    this.selectable = false;
+    this.selectionOfTag = new EventEmitter<Tag>();
+    this.elementTag = new ElementRef('');
+    this.styles = '';
+  }
 
   ngAfterViewInit(): void {
-    if (this.tag.isClickable) {
+    if (this.selectable) {
       this.renderer2.listen(
-        this._elementTag.nativeElement,
+        this.elementTag.nativeElement,
         'click',
         (event: any) => {
           event.preventDefault();
-          if (this._selected === 'has-selected') {
-            this._selected = '';
-          } else {
-            this._selected = 'has-selected';
-            this.selected.emit(true);
-          }
+          this.selectionOfTag.emit(this.tag);
         }
       );
     }
