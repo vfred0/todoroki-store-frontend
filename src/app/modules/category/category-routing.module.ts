e237779178b835fa4336f5ed39a4e7ca03ed6666
Victor Arreaga <arreagav17@gmail.com>
2022-07-18 16:58:29 -0500

    Added page & routing

--- src/app/modules/category/category-routing.module.ts
+++ src/app/modules/category/category-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryPageComponent } from './page/category-page/category-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoryPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/category/category.module.ts
+++ src/app/modules/category/category.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoryRoutingModule } from './category-routing.module';
+import { CategoryPageComponent } from './page/category-page/category-page.component';
+
+import { SwiperModule } from 'swiper/angular';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, CategoryRoutingModule],
+  declarations: [CategoryPageComponent],
+  imports: [CommonModule, CategoryRoutingModule, SwiperModule, SharedModule],
 })
 export class CategoryModule {}
