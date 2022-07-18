b74d6a9316089cabecffe4c7f9e550fd7594ea30
Victor Arreaga <arreagav17@gmail.com>
2022-07-18 17:46:45 -0500

    Refactor

--- src/app/shared/components/color/color.component.ts
+++ src/app/shared/components/color/color.component.ts
@@ -15,33 +15,30 @@ import { Color, TypeColor } from '@core/utils/Color';
   templateUrl: './color.component.html',
 })
 export class ColorComponent implements AfterViewInit {
-  @Input() color: Color = {
-    typeColor: TypeColor.White,
-    pathIcon: 'assets/icons/check.svg',
-    isClickable: true,
-  };
-  @ViewChild('eColor') _elementColor: ElementRef = new ElementRef('');
-  _selected: string = '';
+  @Input() color: Color;
+  @Input() styles: string;
+  @Input() isSelected: boolean;
+  @Output() selectionOfColor: EventEmitter<Color> = new EventEmitter();
+  @ViewChild('elementColor') elementColor: ElementRef = new ElementRef('');
 
-  @Output() selected: EventEmitter<boolean> = new EventEmitter();
-
-  constructor(private renderer2: Renderer2) {}
+  constructor(private renderer2: Renderer2) {
+    this.color = {
+      typeColor: TypeColor.White,
+      pathIcon: 'assets/icons/check.svg',
+      isSelected: false,
+    };
+    this.isSelected = false;
+    this.styles = '';
+  }
 
   ngAfterViewInit(): void {
-    if (this.color.isClickable) {
-      this.renderer2.listen(
-        this._elementColor.nativeElement,
-        'click',
-        (event: any) => {
-          event.preventDefault();
-          if (this._selected === 'has-selected') {
-            this._selected = '';
-          } else {
-            this._selected = 'has-selected';
-            this.selected.emit(true);
-          }
-        }
-      );
-    }
+    this.renderer2.listen(
+      this.elementColor.nativeElement,
+      'click',
+      (event: any) => {
+        event.preventDefault();
+        this.selectionOfColor.emit(this.color);
+      }
+    );
   }
 }
