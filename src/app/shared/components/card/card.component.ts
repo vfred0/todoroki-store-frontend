2e9f6b17964ceadf7b620836980ba8ea7de465a6
Victor Arreaga <arreagav17@gmail.com>
2022-07-18 17:57:26 -0500

    Refactor

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Tag } from '@core/utils/Tag';
 
 @Component({
   selector: 'app-card',
@@ -9,4 +10,8 @@ export class CardComponent {
   @Input() price: number = 13.99;
   @Input() image: string =
     'https://imgs.search.brave.com/V5Y8SJYlqQG79-DGwjAV_qg6RW9G2Jmf8mtWvG6pg4c/rs:fit:861:1200:1/g:ce/aHR0cHM6Ly9zLnlp/bWcuY29tL2FhaC9t/aWxpdGFyeWJlc3Qv/dS1zLW5hdnktdmll/dG5hbS1kZW5pbS1z/aGlydC0yNS5naWY.gif';
+  likes: Tag = {
+    description: '150',
+    pathIcon: 'assets/icons/heart.svg',
+  };
 }
