1f94f023b417c94be61bf5fa9a8cc6060a3fdb0a
Victor Arreaga <arreagav17@gmail.com>
2022-07-20 09:43:54 -0500

    Refactor

--- src/app/shared/components/tag/tag.component.ts
+++ src/app/shared/components/tag/tag.component.ts
@@ -15,24 +15,23 @@ import { Tag } from '@core/utils/Tag';
   templateUrl: './tag.component.html',
 })
 export class TagComponent implements AfterViewInit {
-  @Output() selectionOfTag: EventEmitter<Tag>;
-  @Input() selectable: boolean;
   @Input() tag: Tag;
   @Input() styles: string;
-  @Input() isSelected: boolean;
+  @Input() isSelected: boolean = false;
+  @Input() isSelectable: boolean;
+  @Output() selectionOfTag: EventEmitter<Tag>;
 
   @ViewChild('elementTag') elementTag: ElementRef;
   constructor(private renderer2: Renderer2) {
-    this.isSelected = false;
     this.tag = {} as Tag;
-    this.selectable = false;
+    this.isSelectable = true;
     this.selectionOfTag = new EventEmitter<Tag>();
     this.elementTag = new ElementRef('');
     this.styles = '';
   }
 
   ngAfterViewInit(): void {
-    if (this.selectable) {
+    if (this.isSelectable && !this.isSelected) {
       this.renderer2.listen(
         this.elementTag.nativeElement,
         'click',
