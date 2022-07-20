986c88b79d256112c18d866dbbea7e3ae444a42c
Victor Arreaga <arreagav17@gmail.com>
2022-07-20 11:56:26 -0500

    Refactor

--- src/app/modules/categories/page/categories-page.component.ts
+++ src/app/modules/categories/page/categories-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectorRef,
+  Component,
+  Type,
+} from '@angular/core';
 import { Product } from '@core/models/Product';
 import { TypeAnimes } from '@core/models/TypeAnimes';
 import { TypeClothings } from '@core/models/TypeClothings';
@@ -11,13 +16,20 @@ import { TypeColor } from '@core/utils/TypeColor';
   selector: 'app-category-page',
   templateUrl: './categories-page.component.html',
 })
-export class CategoriesPageComponent {
+export class CategoriesPageComponent implements AfterContentInit {
   sizes: Array<Size>;
   typesAnimes: Array<Option<TypeAnimes>>;
   typesClotings: Array<Option<TypeClothings>>;
   private products: Array<Product>;
+  productsCardsFiltered: Array<Product>;
   colors: TypeColor[];
-  constructor() {
+  size: Size;
+  typeColor: TypeColor;
+  typeAnime: Option<TypeClothings>;
+  typeCloting: Option<TypeClothings>;
+
+  constructor(private cd: ChangeDetectorRef) {
+    this.productsCardsFiltered = [];
     this.typesAnimes = [];
     this.typesClotings = [];
     this.products = [];
@@ -32,27 +44,74 @@ export class CategoriesPageComponent {
     this.sizes = [...Object.values(Size)];
     this.products = [];
 
+    this.size = Size.S;
+    this.typeColor = TypeColor.Black;
+    this.typeAnime = TypeAnimes.Anime1;
+    this.typeCloting = TypeClothings.Shirt;
+  }
+  ngAfterContentInit(): void {
+    this.products = [
+      {
+        description: 'Camiseta1',
+        color: TypeColor.Black,
+        size: Size.S,
+        typesAnimes: TypeAnimes.Anime,
+        typeClothing: TypeClothings.Shirt,
+        price: 10,
+        image: 'https://picsum.photos/200/300',
+        likes: 0,
+      },
+      {
+        description: 'Camiseta2',
+        color: TypeColor.White,
+        size: Size.M,
+        typesAnimes: TypeAnimes.Anime1,
+        typeClothing: TypeClothings.Shirt,
+        price: 100,
+        image: 'https://picsum.photos/200/300',
+        likes: 0,
+      },
+    ];
+    // this.productsCardsFiltered = this.products.filter(
+    //   (product: Product) =>
+    //     product.color === TypeColor.Black &&
+    //     product.size === Size.S &&
+    //     product.typesAnimes === TypeAnimes.Anime &&
+    //     product.typeClothing === TypeClothings.Shirt
+    // );
+
+    // console.log(this.productsCardsFiltered);
+    this.cd.detectChanges();
+  }
+  setProductsFiltered() {
+    this.productsCardsFiltered = this.products.filter(
+      (product: Product) =>
+        product.color === this.typeColor &&
+        product.size === this.size &&
+        product.typesAnimes === this.typeAnime &&
+        product.typeClothing === this.typeCloting
+    );
+  }
+  getProductsCardsFiltered() {
+    return this.productsCardsFiltered;
+  }
+
+  updateProductsByAnime(value: Option<TypeAnimes>) {
+    this.typeAnime = value;
     this.setProductsFiltered();
   }
-  setProductsFiltered() {}
-
-  existsProductsFiltered() {}
-  getProductsFiltered() {
-    return [];
-  }
-  updateProductsByAnime(value: Option<TypeAnimes>) {
-    console.log(Object.is(value, TypeAnimes.Anime));
-    console.log(value);
-  }
   updateProductsByCloth(value: Option<TypeClothings>) {
-    console.log(value);
+    this.typeCloting = value;
+    this.setProductsFiltered();
   }
 
   updateProductsByColor(value: TypeColor) {
-    console.log(value);
+    this.typeColor = value;
+    this.setProductsFiltered();
   }
 
   updateProductsBySize(size: Size) {
-    console.log(size);
+    this.size = size;
+    this.setProductsFiltered();
   }
 }
