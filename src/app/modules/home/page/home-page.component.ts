23fbc22c1e416e74471a8953733358e9b3adcd8b
Victor Arreaga <arreagav17@gmail.com>
2022-07-21 11:45:42 -0500

    Refactor

--- src/app/modules/home/page/home-page.component.ts
+++ src/app/modules/home/page/home-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductItemCart } from '@core/utils/ProductItemCart';
+import { Size } from '@core/utils/Size';
+import { TypeColor } from '@core/utils/TypeColor';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent {
-  valueChanged(value: number) {
-    console.log(value);
-  }
-}
+export class HomePageComponent {}
