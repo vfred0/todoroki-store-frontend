8597af53767acc459e57ca252807ba481e9cb1a7
Victor <arreagav17@gmail.com>
2022-08-30 07:36:50 -0500

    Added responsabilities

--- src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-order-status/select-order-status.component.ts
+++ src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-order-status/select-order-status.component.ts
@@ -2,7 +2,6 @@ import {
   Component,
   EventEmitter,
   Input,
-  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
@@ -14,26 +13,32 @@ import { SelectComponent } from '@shared/components/select/select.component';
   selector: 'app-select-order-status',
   templateUrl: './select-order-status.component.html',
 })
-export class SelectOrderStatusComponent implements OnInit {
-  @ViewChild(SelectComponent) selectOrderStatus!: SelectComponent;
+export class SelectOrderStatusComponent {
   optionsOrderStatus: Array<Option<OrderStatus>>;
   @Output() updateOrderStatus: EventEmitter<OrderStatus>;
   @Input() orderStatusSelected: OrderStatus;
+  @ViewChild(SelectComponent) selectComponent!: SelectComponent;
 
   constructor() {
     this.optionsOrderStatus = [];
     Object.values(OrderStatus).forEach((value: string) =>
       this.optionsOrderStatus.push(value)
     );
-    this.orderStatusSelected = OrderStatus.Sold;
+    this.orderStatusSelected = {} as OrderStatus;
     this.updateOrderStatus = new EventEmitter<OrderStatus>();
   }
 
-  ngOnInit(): void {
-    console.log('OPTIONS XX SELECTED', this.orderStatusSelected);
+  update(orderStatus: Option<OrderStatus>) {
+    this.updateOrderStatus.emit(
+      Object.keys(OrderStatus)[
+        Object.values(OrderStatus).indexOf(orderStatus as OrderStatus)
+      ] as OrderStatus
+    );
   }
 
-  update(optionAnimes: Option<OrderStatus>) {
-    this.updateOrderStatus.emit(optionAnimes as OrderStatus);
+  setOptionSelected(orderStatus: OrderStatus) {
+    this.selectComponent.setOptionSelected(
+      Object.values(OrderStatus)[Object.keys(OrderStatus).indexOf(orderStatus)]
+    );
   }
 }
--- src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-payment-type/select-payment-type.component.ts
+++ src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-payment-type/select-payment-type.component.ts
@@ -1,6 +1,14 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { PaymentType } from '@core/types/PaymentType';
 import { Option } from '@core/utils/Option';
+import { SelectComponent } from '@shared/components/select/select.component';
 
 @Component({
   selector: 'app-select-payment-type',
@@ -9,17 +17,27 @@ import { Option } from '@core/utils/Option';
 export class SelectPaymentTypeComponent {
   optionsPaymentType: Array<Option<PaymentType>>;
   @Output() updatePaymentType: EventEmitter<PaymentType>;
+  @ViewChild(SelectComponent) selectComponent!: SelectComponent;
+
+  setOptionSelected(paymentType: PaymentType) {
+    this.selectComponent.setOptionSelected(
+      Object.values(PaymentType)[Object.keys(PaymentType).indexOf(paymentType)]
+    );
+  }
 
   constructor() {
     this.optionsPaymentType = [];
     Object.values(PaymentType).forEach((value: string) =>
       this.optionsPaymentType.push(value)
     );
-
     this.updatePaymentType = new EventEmitter<PaymentType>();
   }
 
-  update(optionAnimes: Option<PaymentType>) {
-    this.updatePaymentType.emit(optionAnimes as PaymentType);
+  update(paymentType: Option<PaymentType>) {
+    this.updatePaymentType.emit(
+      Object.keys(PaymentType)[
+        Object.values(PaymentType).indexOf(paymentType as PaymentType)
+      ] as PaymentType
+    );
   }
 }
