3cb599f697b86e2f8a83a5ff4e796034d8f670e6
Victor <arreagav17@gmail.com>
2022-08-30 07:35:27 -0500

    Select Order component disabled orderDate

--- src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-order-dates/select-order-dates.component.ts
+++ src/app/pages/store-management/pages/orders-management/pages/order-details/components/select-order-dates/select-order-dates.component.ts
@@ -1,48 +1,20 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { SelectOrderDateComponent } from '../../../order-lists/components/select-order-date/select-order-date.component';
 
 @Component({
   selector: 'app-select-order-dates',
   templateUrl: './select-order-dates.component.html',
 })
-export class SelectOrderDatesComponent extends SelectOrderDateComponent {
-  // formGroup: FormGroup;
-  // @Output() updateOrderDates: EventEmitter<Map<string, string>>;
-  // constructor() {
-  //   super();
-  //   this.updateOrderDates = new EventEmitter<Map<string, string>>();
-  // }
-  //   this.filterOrder = new EventEmitter<Map<string, string>>();
-  //   this.formGroup = new FormGroup({
-  //     orderDate: new FormControl(this.getDateNow(), Validators.required),
-  //     saleDate: new FormControl(this.getDateNow(), Validators.required),
-  //   });
-  //   this.formGroup.get('orderDate')!.valueChanges.subscribe(value => {
-  //     this.notifyUpdateOrderDateInterval();
-  //   });
-  //   this.formGroup.get('saleDate')!.valueChanges.subscribe(value => {
-  //     this.notifyUpdateOrderDateInterval();
-  //   });
-  // }
-  // ngOnInit(): void {
-  //   this.notifyUpdateOrderDateInterval();
-  // }
-  // getDateNow(): string {
-  //   return new Date().toISOString().split('T')[0];
-  // }
-  // getFormatDate(date: string): string {
-  //   return new Date(date).toISOString().split('T')[0];
-  // }
-  // notifyUpdateOrderDateInterval() {
-  //   this.filterOrder.emit(
-  //     new Map<string, string>([
-  //       [
-  //         'orderDate',
-  //         this.getFormatDate(this.formGroup.get('orderDate')?.value),
-  //       ],
-  //       ['saleDate', this.getFormatDate(this.formGroup.get('saleDate')?.value)],
-  //     ])
-  //   );
-  // }
+export class SelectOrderDatesComponent
+  extends SelectOrderDateComponent
+  implements OnInit
+{
+  constructor() {
+    super();
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.formGroup.get(this.MIN_DATE)!.disable();
+  }
 }
