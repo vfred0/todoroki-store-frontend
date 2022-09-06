f7b96ee6d9f25f7bcbe41c1dc920c4fb4f932920
Victor <arreagav17@gmail.com>
2022-09-06 10:38:31 -0500

    Added services

--- src/app/shared/components/header/components/modal-shopping-cart/modal-shopping-cart.component.ts
+++ src/app/shared/components/header/components/modal-shopping-cart/modal-shopping-cart.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Order } from '@core/models/Order';
 import { ProductItemCart } from '@core/utils/ProductItemCart';
+import { ProductOrder } from '@core/utils/ProductOrder';
+import { OrderService } from '@shared/services/order.service';
 import { ShoppingCartService } from '@shared/services/shopping-cart.service';
+import { Subscription, tap } from 'rxjs';
 import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-modal-shopping-cart',
   templateUrl: './modal-shopping-cart.component.html',
 })
-export class ModalShoppingCartComponent extends ModalComponent {
+export class ModalShoppingCartComponent
+  extends ModalComponent
+  implements OnDestroy
+{
   cartItems$ = this.shoppingcartService.productItemsCart$;
   total$ = this.shoppingcartService.total$;
   itemsCount$ = this.shoppingcartService.productItemsCount$;
-  constructor(private shoppingcartService: ShoppingCartService) {
+  private subscription: Subscription;
+
+  constructor(
+    private orderService: OrderService,
+    private shoppingcartService: ShoppingCartService
+  ) {
     super();
+    this.subscription = new Subscription();
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   removeProductItemCart(productItemCart: ProductItemCart) {
@@ -20,10 +36,39 @@ export class ModalShoppingCartComponent extends ModalComponent {
   }
 
   contactForWhatsapp() {
-    console.log('DEBUG ==>', this.cartItems$);
-    // window.open(
-    //   'https://api.whatsapp.com/send?phone=5511999999999&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20pedido%20de%20produtos%20no%20site%20do%20restaurante%20'
-    // );
+    let productOrders: ProductOrder[] = [];
+    let order: Order = {} as Order;
+    this.subscription.add(
+      this.cartItems$.subscribe(items => {
+        items.forEach(item => {
+          productOrders.push({
+            productId: item.id,
+            color: item.color,
+            size: item.size,
+            price: item.price,
+            quantity: item.quantity,
+          });
+        });
+      })
+    );
+
+    this.subscription.add(
+      this.total$.subscribe(total => (order.price = total))
+    );
+
+    this.subscription.add(
+      this.itemsCount$.subscribe(count => (order.quantity = count))
+    );
+
+    order.productOrders = productOrders;
+    console.log(order);
+
+    this.orderService.save(order).subscribe((orderNumber: number) => {
+      let message: string = `Hola, mi número de orden es ${orderNumber}`;
+      window.open(
+        `https://api.whatsapp.com/send?phone=593939076291&text=${message}`
+      );
+    });
   }
 
   existsProductsInCart(): boolean {
