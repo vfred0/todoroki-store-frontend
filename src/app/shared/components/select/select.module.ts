b7407364c3d75b1356814089504bcfc2a476c3cd
marcosdaniel2002 <mteranc@unemi.edu.ec>
2022-09-06 11:08:12 -0500

    styles for the page product and modal cart

--- src/app/shared/components/select/select.module.ts
+++ src/app/shared/components/select/select.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SelectComponent } from './select.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularSvgIconModule} from 'angular-svg-icon';
 
 @NgModule({
   declarations: [SelectComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AngularSvgIconModule.forRoot()],
   exports: [SelectComponent],
 })
 export class SelectModule {}
